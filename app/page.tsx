f248bdeed3e4cc4a3fc670d1cf49bc4032e37e03
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-09-23 22:49:42 +0330

    better movement and drag the poster

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import next from "next";
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import Poster from "@/public/poster/Kouman-poster.png";
@@ -6,23 +7,54 @@ import Logo from "@/public/png/kouman.png";
 import Twitter from "@/public/png/twitter.png";
 import Instagram from "@/public/png/instagram.png";
 import Youtube from "@/public/png/youtube.png";
+import { motion } from "framer-motion";
 
 
 export default function Home() {
   return (
     <div>
-      <div className="w-[750px] bg-orange-200 rounded-full mx-auto mt-10">
-        <Image
-          className="animate-[floatBalanced_5s_ease-in-out_infinite]"
-          src={Poster}
-          alt="Kouman Poster"
-        />
+      <div className="w-[750px] bg-orange-200 rounded-full mx-auto mt-10 flex justify-center items-center">
+        <motion.div
+          drag
+          dragElastic={0.2}
+          dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+          className="cursor-grab active:cursor-grabbing"
+          animate={{
+            y: [0, -20, 0, 20, 0],
+            scale: [1, 1.04, 1, 1.04, 1]
+          }}
+          transition={{
+            y: {
+              repeat: Infinity,
+              duration: 6,
+              ease: "easeInOut",
+            },
+            scale: {
+              repeat: Infinity,
+              duration: 6,
+              ease: "easeInOut",
+            },
+            type: "spring",
+            stiffness: 200,
+            damping: 20,
+          }}
+        >
+          <Image
+            src={Poster}
+            alt="Kouman Poster"
+            className="select-none pointer-events-none"
+          />
+        </motion.div>
       </div>
 
       <div className="m-20 text-center">
         <div className="flex flex-col gap-5">
           <h1 className="text-2xl font-black">جمعه‌های نمکی با کومان!!!؟</h1>
-          <p className="text-xl">برنامه‌ی <strong className="text-2xl">کومان</strong> جاییه که هر جمعه‌ کورش، ایمان و میا با یه چالش جدید غیرمنتظره شما رو همراه خودشون می‌برن <br /> تا هر‌جور غذایی که فکرشو بکنین بخورن، عجیب‌ترین محصولات اینترنتی رو امتحان کنن، به تاریخ ایران زمین سفر کنن <br /> و تو فرهنگ غنیش جست و جو کنن و تو بازی‌های مختلف با هم مسابقه بدن!</p>
+          <p className="text-xl">
+            برنامه‌ی <strong className="text-2xl">کومان</strong> جاییه که هر جمعه‌ کورش، ایمان و میا با یه چالش جدید غیرمنتظره شما رو همراه خودشون می‌برن <br />
+            تا هر‌جور غذایی که فکرشو بکنین بخورن، عجیب‌ترین محصولات اینترنتی رو امتحان کنن، به تاریخ ایران زمین سفر کنن <br />
+            و تو فرهنگ غنیش جست و جو کنن و تو بازی‌های مختلف با هم مسابقه بدن!
+          </p>
         </div>
       </div>
 
@@ -30,27 +62,15 @@ export default function Home() {
         <div className="flex flex-col gap-10 items-end">
           <h2 className="flex gap-2 items-center">
             <Link href="https://x.com/thekouman">Twitter</Link>
-            <Image
-              src={Twitter}
-              className="w-7"
-              alt="Kouman Twitter"
-            />
+            <Image src={Twitter} className="w-7" alt="Kouman Twitter" />
           </h2>
           <h2 className="flex gap-2 items-center">
             <Link href="https://instagram.com/kouman">Instagram</Link>
-            <Image
-              src={Instagram}
-              className="w-7"
-              alt="Kouman Instagram"
-            />
+            <Image src={Instagram} className="w-7" alt="Kouman Instagram" />
           </h2>
           <h2 className="flex gap-2 items-center">
             <Link href="https://www.youtube.com/@Kouman">Youtube</Link>
-            <Image
-              src={Youtube}
-              className="w-7"
-              alt="Kouman Youtube"
-            />
+            <Image src={Youtube} className="w-7" alt="Kouman Youtube" />
           </h2>
         </div>
 
