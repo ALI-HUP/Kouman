0eb86e41f48cfc87622fb6221d15cf1b571696b0
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-09-23 19:09:51 +0330

    cleaning

--- app/universe/page.tsx
+++ app/universe/page.tsx
@@ -1,10 +1,4 @@
 import next from "next";
-import Image from "next/image";
-import Poster from "@/public/poster/Kouman-poster.png";
-import Logo from "@/public/png/kouman.png";
-import Twitter from "@/public/png/twitter.png";
-import Instagram from "@/public/png/instagram.png";
-import Youtube from "@/public/png/youtube.png";
 
 
 export default function Universe() {
